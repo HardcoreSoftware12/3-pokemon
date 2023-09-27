838d05a003660bfdcd7be1a736c118499e22c41f
HardcoreSoftware12 <devadigaranjith12@gmail.com>
2023-09-27 12:12:42 +0530

    display and fetch

--- index.js
+++ index.js
@@ -1,4 +1,46 @@
+console.log('hellp');
 
-const fight = document.getElementById('fight')
-console.log('');
+const fight  = document.getElementById('fight')
+// console.log(fight);
+fight.addEventListener('click',()=>{
+    getPokemon();
+})
 
+function getPokemon(){
+    const req = fetch(`https://pokeapi.co/api/v2/pokemon/`)
+    
+   req.then((response)=>{console.log(response);
+   
+            return response.json()})
+            .then((pokemon)=>{
+                console.log(pokemon.results);
+                displayPlayer1(pokemon.results)
+                // fetch(pokemon.results[2].url)
+                // .then((response)=>{return response.json()})
+                // .then((singlepoke)=>{console.log(singlepoke);
+                //                      console.log(singlepoke.abilities[1].ability.name);
+                
+                
+                
+                
+                })
+                console.log();
+
+            // })
+}
+function displayPlayer1(results){
+    const topHeader = document.getElementById('card_header')
+   
+    const player1 = document.getElementById('p1_name')
+    // let score1 = topHeader.getElementById('p1_score')
+
+
+    
+   
+    
+    player1.textContent ='hello';
+    console.log(results);
+    let randomNum = Math.floor(((Math.random()*20)))
+    console.log(results[randomNum].name);
+
+}
